b0f39abbd4252729230134f3e189c64cb546c5c4
infotechnab <infotechnab@gmail.com>
2014-04-18 19:31:21 +0545

    Coming Soon updated

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -70,16 +70,9 @@
 	<h1>Welcome to Online Reservation System</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
+	
+            <p>We are coming very soon. Just keep your eyes open. :)</p>
+        </div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
 </div>
