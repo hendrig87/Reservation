79b720cd3fcf40ff0856c44c7e5cec7610507e36
bhomnath <bhomnath@outlook.com>
2014-05-02 11:26:31 +0545

    database file updated

--- database/reservation.sql
+++ database/reservation.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 30, 2014 at 05:01 AM
+-- Generation Time: May 02, 2014 at 05:40 AM
 -- Server version: 5.6.12-log
 -- PHP Version: 5.4.12
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `reservation`
 --
-CREATE DATABASE IF NOT EXISTS `reservation` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `reservation` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `reservation`;
 
 -- --------------------------------------------------------
@@ -29,18 +29,39 @@ USE `reservation`;
 --
 
 CREATE TABLE IF NOT EXISTS `hotel_info` (
-  `hotel_id` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL,
-  `address` varchar(100) NOT NULL,
-  `contact` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) CHARACTER SET utf8 NOT NULL,
+  `address` varchar(200) CHARACTER SET utf8 NOT NULL,
+  `contact` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `hotel_info`
 --
 
-INSERT INTO `hotel_info` (`hotel_id`, `name`, `address`, `contact`) VALUES
-(1, 'everest', 'jkdhfka', '687576');
+INSERT INTO `hotel_info` (`id`, `name`, `address`, `contact`, `user_id`) VALUES
+(1, 'everest', 'pulchowk', 'bdhjgsagdjh', 7),
+(2, 'nandan', 'ysdghj', 'dvhg', 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_info`
+--
+
+CREATE TABLE IF NOT EXISTS `personal_info` (
+  `Id` int(5) NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(200) CHARACTER SET utf8 NOT NULL,
+  `address` varchar(300) CHARACTER SET utf8 NOT NULL,
+  `occupation` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `nationality` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `contact_no` int(20) NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `remarks` varchar(500) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -57,7 +78,7 @@ CREATE TABLE IF NOT EXISTS `room_registration` (
   `image` varchar(255) NOT NULL,
   `hotel_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=67 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=77 ;
 
 --
 -- Dumping data for table `room_registration`
@@ -65,7 +86,17 @@ CREATE TABLE IF NOT EXISTS `room_registration` (
 
 INSERT INTO `room_registration` (`id`, `room_name`, `no_of_room`, `price`, `description`, `image`, `hotel_id`) VALUES
 (65, 'Deluxe', 10, 800, 'rybsvssfd', '2P2Z4.png', 1),
-(66, 'Luxury', 12, 1000, 'dfh dvsrtruub  dsgd', 'arrow.jpg', 1);
+(66, 'Luxury', 12, 1000, 'dfh dvsrtruub  dsgd', 'arrow.jpg', 1),
+(67, '', 1, 0, '', '', 0),
+(68, '', 1, 0, '', '', 0),
+(69, '', 1, 0, '', '', 0),
+(70, 'asdas', 4, 500, 'sadsadasdas', 'DSCF3263.jpg', 0),
+(71, '0', 0, 0, '0', '', 0),
+(72, 'sdafdsdfsaa', 6, 2500, 'sadadas', '', 0),
+(73, '0', 0, 0, '0', '', 2),
+(74, '0', 0, 0, '0', '', 2),
+(75, '0', 0, 0, '0', '', 1),
+(76, 'My Room available', 7, 1233, '213213123', '', 2);
 
 -- --------------------------------------------------------
 
@@ -83,7 +114,7 @@ CREATE TABLE IF NOT EXISTS `user_info` (
   `user_auth_key` varchar(64) NOT NULL,
   `hotel_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
 
 --
 -- Dumping data for table `user_info`
@@ -94,7 +125,9 @@ INSERT INTO `user_info` (`id`, `user_name`, `user_fname`, `user_lname`, `user_em
 (4, '\\sa', 'AS', 'A', 'A@akjs.ud', '8c80b057bc0b599b48cbd144558aeada', '', 0),
 (5, 'admin', 'homnath', 'bagale', 'bhomnath@salyani.com', '21232f297a57a5a743894a0e4a801fc3', '1SYPD8XIWJ', 0),
 (6, 'sushil', 'shrerha', 'lakfals', 'sushilsth21@gmail.co', '698d51a19d8a121ce581499d7b701668', '', 4),
-(7, 'ksadfjlsafj', 'sadfkl', 'kldgjsdkl', 'sunil@gmail.com', '912ec803b2ce49e4a541068d495ab570', '', 1);
+(7, 'ksadfjlsafj', 'sadfkl', 'kldgjsdkl', 'sunil@gmail.com', '912ec803b2ce49e4a541068d495ab570', '', 1),
+(8, 'jay', 'jayash', 'gsfdatgds', 'jayash@gmail.com', '912ec803b2ce49e4a541068d495ab570', '', 0),
+(9, 'mynba', 'hsdydhas', 'joidjsaui', 'asdsy@gsuyguu.asyfg', '7f2b3b81e81984b415a16d0400388eda', '', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
