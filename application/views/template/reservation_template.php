64ef638c3469c59f00e70f25bb461f80e1e57f0f
Ramji Subedi <ramjisubedi1991@yahoo.com>
2014-05-07 13:47:45 +0545

    add select in the list of children option

--- application/views/template/reservation_template.php
+++ application/views/template/reservation_template.php
@@ -89,9 +89,9 @@ setInterval("displaytime()", 1000);
                              <span class="add-on">Children</span>
                        
                              <select name="children" required id="child" style="border-radius:0px 5px 5px 0px;">
-
+                                 <option > Select</option>
                             <?php
-                            for ($i = 0; $i <= $children; $i++) {
+                            for ($i = 1; $i <= $children; $i++) {
                                 echo "<option value=" . $i . ">" . $i . "</option>";
                             }
                             ?>
