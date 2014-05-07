c8c80d0d1dc7052e4de41647514e52e88ca03a29
Ramji Subedi <ramjisubedi1991@yahoo.com>
2014-05-07 17:41:51 +0545

    test

--- application/controllers/room_booking.php
+++ application/controllers/room_booking.php
@@ -40,7 +40,7 @@ class room_booking extends CI_Controller {
             $hotelId= $_POST['hotelId'];
             $data['query']= $this->dashboard_model->booking_room($hotelId);
             //$data['total_room']= $this->dashboard_model->total_room();
-            $data['queryForAvailableRoom'] = $this->dashboard_model->availableRoom($checkIn,$checkOut);
+           // $data['queryForAvailableRoom'] = $this->dashboard_model->availableRoom($checkIn,$checkOut);
             
             //$data['booked_room']= $this->dashboard_model->booked_room();
               $data['json'] = json_encode($data['query']);
