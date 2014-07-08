807d272290dd11a9992e9c7331884f2ef405e931
bhomnath <bhomnath@outlook.com>
2014-07-08 17:12:08 +0545

    private.xml

--- application/views/ReservationInformation/bookedRoomInformation.php
+++ application/views/ReservationInformation/bookedRoomInformation.php
@@ -10,7 +10,7 @@
     margin: 1em 0; padding: 0;">
      <div id="room_book">
    <?php
-    if(!empty($roomInfo))
+    if(isset($roomInfo))
     { ?>
     <table width="100%">
         <tr>
@@ -19,9 +19,8 @@
             <th width="15%">From</th>
             <th width="15%">To</th>
             <th width="10%">No. of individuals</th>
-            <th width="20%">Contact Person/ Address</th>
+            <th width="20%">Contact Person</th>
             <th width="15%">Contact Number</th>
-             <th width="15%">Booked Hotel Info</th>
             
     <?php
    
@@ -31,8 +30,7 @@
             $noOfRooms = $book->no_of_rooms_booked;
             $checkIn= $book->check_in_date;
             $checkOut = $book->check_out_date;
-            $bookingId= $book->booking_id;
-            $hotelId = $book->hotel_id;
+            $personalInfoId = $book->personal_info_id;
             
             
     ?>
@@ -50,7 +48,7 @@
             </td>
             <td> <?php echo $checkOut; ?></td>
             
-            <?php $personalInfo = $this->dashboard_model->get_booking_personal_info($bookingId);
+            <?php $personalInfo = $this->dashboard_model->get_booking_personal_info($personalInfoId);
     if(!empty($personalInfo))
     {        foreach ($personalInfo as $bookPerson){
         $bookingName= $bookPerson->full_name;
@@ -63,23 +61,13 @@
         
         ?>
         <td> <?php echo $totalPupil; ?></td>
-       <td> <?php echo $bookingName."<br>". $bookingEmail."<br>".$bookAddress; ?></td>
-        <td> <?php echo $contact ; ?></td>
-        
+       <td> <?php echo $bookingName."<br>". $bookingEmail; ?></td>
+        <td> <?php echo $bookAddress."<br>".$contact ; ?></td>
         
     <?php }} ?>
-        <?php $hotelInfo = $this->dashboard_model->get_hotel_info($hotelId);
-        if(!empty($hotelInfo))
-    {        foreach ($hotelInfo as $bookHotel){
-        $hotelName= $bookHotel->name;
-        $hotelAddress = $bookHotel->address;
-        $contact = $bookHotel->contact;
-    }}
-        ?>
-        <td><?php echo $hotelName."<br>". $hotelAddress."<br>".$contact;  ?></td>
   <!--          <td>    
-                <?php// echo anchor('dashboard/edit/'.$book->id,'<img src="'.  base_url().'contents/images/edit.png" height="20px" width="20px" alt="Edit" id="edit_room">'); ?>&nbsp;&nbsp;&nbsp;
-                <?php// echo anchor('dashboard/delete/'.$book->id,'<img src="'.  base_url().'contents/images/delete.jpg" height="20px" width="20px" alt="Delete" id="delete_room">'); ?>
+                <?php echo anchor('dashboard/edit/'.$book->id,'<img src="'.  base_url().'contents/images/edit.png" height="20px" width="20px" alt="Edit" id="edit_room">'); ?>&nbsp;&nbsp;&nbsp;
+                <?php echo anchor('dashboard/delete/'.$book->id,'<img src="'.  base_url().'contents/images/delete.jpg" height="20px" width="20px" alt="Delete" id="delete_room">'); ?>
                 
             </td>  -->
             
@@ -88,9 +76,6 @@
     <?php           
     }
     }
- else {
-        echo '<h3>Sorry! no rooms are booked</h3>';        
-}
     ?>
         </tr>
     </table>
