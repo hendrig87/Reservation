e2ab745b1a6b817e1c80b9ae30ba580b50801e10
Ramji Subedi <ramjisubedi1991@yahoo.com>
2014-08-18 13:44:29 +0545

    padding in booking view

--- application/views/ReservationInformation/bookedRoomInfoAjax.php
+++ application/views/ReservationInformation/bookedRoomInfoAjax.php
@@ -2,19 +2,16 @@
    <?php
     if(!empty($roomInfo))
     { ?>
-    <table width="100%" style="border-collapse: collapse">
+     <table width="100%" style="border-collapse: collapse">
         <tr>
             <th width="17%">Room</th>
-            <th width="7%">No.of room</th>
+            <th width="7%">No. of room</th>
            
-            <th width="9%">From</th>
-            <th width="9%">To</th>
-            <th width="10%">Total Days</th>
+            <th width="12%">From - To</th>
             <th width="10%">Remain days</th>
-            <th width="9%">No. of individuals</th>
-            <th width="15%">Contact Person/ Address</th>
-            <th width="10%">Contact Number</th>
-             <th width="20%">Booked Hotel Info</th>
+            <th width="9%">No. of Pupil</th>
+            <th width="25%">Contact Person/ Address</th>
+             <th width="15%">Booked Hotel</th>
              <th width="4%">Action</th>
         </tr>  
     <?php
@@ -27,19 +24,21 @@
             $checkOut = $book->check_out_date;
             $bookingId= $book->booking_id;
             $hotelId = $book->hotel_id;
-
+           
             $currentDate = date("Y-m-d");
     $days = floor( ( strtotime( $checkOut ) - strtotime(  $checkIn ) ) / 86400 );
     $remain = floor( ( strtotime( $checkIn ) - strtotime(  $currentDate ) ) / 86400 );
             
+
+            
          $bookedRoomInfo= $this->dashboard_model->get_all_booked_room_info($bookingId);
      
-                 
+                
     if ($checkIn <= $currentDate && $checkOut >= $currentDate)
         { ?>
-          <tr class="current" >
+        <tr class="current" style="border-bottom:1px solid #ccc;" >
         <?php } else { ?>
-       <tr class="upcomming">
+       <tr class="upcomming" style="border-bottom:1px solid #ccc;">
   <?php  }   ?>
         
             <td>
@@ -62,9 +61,9 @@
         <?php echo $noOfRooms;?> <br/> <?php }?> </td>
             
             <td>
-                <?php echo $checkIn; ?>
+                <?php echo $checkIn.' to '.$checkOut.'<br/>('.$days.' days)'; ?>
             </td>
-            <td> <?php echo $checkOut; ?></td>
+            
             
             <?php $personalInfo = $this->dashboard_model->get_booking_personal_info($bookingId);
     if(!empty($personalInfo))
@@ -78,11 +77,11 @@
         $totalPupil = $child + $adult;
         
         ?>
-            <td><?php if($days>1){ echo $days." days";}else{ echo $days." day";} ?></td>
-            <td><?php if($remain>1){ echo $remain." days";}else{ echo $remain." day";} ?></td>
+<!--            <td><?php// if($days>1){ echo $days." days";}else{ echo $days." day";} ?></td>-->
+            <td><?php if($remain>=1){ echo $remain." days";}else{ echo "currently running";} ?></td>
         <td> <?php echo $totalPupil; ?></td>
-        <td> <?php echo $bookingName."<br>". $bookingEmail."<br>".$bookAddress; ?></td>
-        <td> <?php echo $contact ; ?></td>
+        <td> <?php echo $bookingName."<br>". $bookingEmail."<br>".$bookAddress."<br>".$contact; ?></td>
+       
         
         
     <?php }} ?>
@@ -94,10 +93,10 @@
         $contact = $bookHotel->contact;
     
         ?>
-        <td><?php echo $hotelName."<br>". $hotelAddress."<br>".$contact;  ?></td> <?php }} else{ ?><td><?php echo 'hotel not found';  ?></td><?php } ?>
+        <td><?php echo $hotelName;  ?></td> <?php }} else{ ?><td><?php echo 'hotel not found';  ?></td><?php } ?>
    <td>    
-                <?php echo anchor('dashboard/edit/'.$book->id,'<img src="'.  base_url().'contents/images/edit.png"  alt="Edit" class="edit_room">'); ?>&nbsp;&nbsp;&nbsp;
-                <?php echo anchor('dashboard/delete/'.$book->id,'<img src="'.  base_url().'contents/images/delete.png" alt="Delete" class="delete_room">'); ?>
+                <?php echo anchor('dashboard/editBooking/'.$book->id,'<img src="'.  base_url().'contents/images/edit.png"  alt="Edit" class="edit_room">'); ?>&nbsp;&nbsp;&nbsp;
+                <?php echo anchor('dashboard/deleteBooking/'.$book->id,'<img src="'.  base_url().'contents/images/delete.png" alt="Delete" class="delete_room">'); ?>
                 
             </td>  
             
