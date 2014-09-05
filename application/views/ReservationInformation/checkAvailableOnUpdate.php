c0a3761440e6ce8fb337f958b32e579298016efe
bhomnath <bhomnath@outlook.com>
2014-09-05 13:13:06 +0545

    user stopped if no rooms are selected

--- application/views/ReservationInformation/checkAvailableOnUpdate.php
+++ application/views/ReservationInformation/checkAvailableOnUpdate.php
@@ -1,7 +1,6 @@
 <?php $this->load->helper('availableroom');
 $this->load->helper('currency');   ?>
 <script src="<?php echo base_url() . "contents/scripts/jquery.js"; ?>"></script>
-<script src="<?php echo base_url() . "contents/scripts/datepicker.js"; ?>"></script>
 <script>
     
     var txtnext;
@@ -14,14 +13,14 @@ $this->load->helper('currency');   ?>
     $(document).ready(function() {
          
         $('.available-room').change(function() {            //action performs when no of  rooms is selected
-           alert('here');
+          
             $("#disablebtnInfo").hide();
 
             var rooms = $(this).val();
 
             var price = $(this).parent().prev('td').children('span.priceTag').text();
-            var total = rooms * price;
-            $(this).parent().next('td').children('span.subTotal').text(total);
+            var total = "Rs." + rooms * price;
+            $(this).parent().next('td').text(total);
             
         });
 
@@ -40,8 +39,8 @@ $this->load->helper('currency');   ?>
                         room_name + '</div><br></td><td>' +
                         desc + '</td><td>' +
                         roomprice + '</td><td><select name="selectMe[]" id="roomToBook" class="available-room" style="width: 80px;"><option>' +
-                        rooms +'</option></td><td>' +
-                        total +'</td><td><img class="remove" src="<?php echo base_url() . 'contents/images/subtract.png'; ?>" width="30" height="30"></td></tr>';
+                        rooms +'</option></td><td><span class="subTotal">' +
+                        total +'</span></td><td><img class="remove" src="<?php echo base_url() . 'contents/images/subtract.png'; ?>" width="30" height="30"></td></tr>';
                 
             if(rooms == 0){ 
                     $("#disablebtnInfo").html('<span class="error_sign">!</span>&nbsp;' + 'Please select the rooms & click +');
@@ -66,7 +65,7 @@ $this->load->helper('currency');   ?>
    var price = $(this).parent().prev().prev().prev('td').text();
    var roomprice = price.replace( /^\D+/g, '');;
    var rooms= $(this).parent().prev().prev('td').find('select').val();
-   var total= 'Rs. 0.00';
+   var total= 'Rs. 0';
    
    var data ='<tr style="border-bottom:1px solid #ccc;" id="' + room_id + '"><input type="hidden" name="hidden[]" value="' + room_id + '" /><td><div style="float: left; margin-right: 10px;"><img src="' +image +'" width="50px" height="50px"></div><div style="font-size: 16px;width: 60%; float: left;" id="room-name">' +
                         room_name + '</div><br></td><td>' +
@@ -97,7 +96,9 @@ $this->load->helper('currency');   ?>
  
  
         $("#updatedBooking").click(function(e) {
-            if (('#disablebtn').val() == 'yes')
+        var a =  $(".subTotal").text();  
+        alert(a);
+            if ( a == 0)
             {
                 e.preventDefault();
                 $("#disablebtnInfo").html('<span class="error_sign">!</span>&nbsp;' + 'Please select the rooms & click +');
@@ -115,13 +116,13 @@ $this->load->helper('currency');   ?>
 // iterate through each td based on class and add the values
     $(".subTotal").each(function() {
     var value = $(this).text();
+    
     // add only if the value is number
     if (!isNaN(sum) && sum.length != 0) {
         sum += parseFloat(value);
-         $('#disablebtn').val('no');
+        // $('#disablebtn').val('no');
     }
-    else{
-         $('#disablebtn').val('yes'); }
+   
     });
 
 }
